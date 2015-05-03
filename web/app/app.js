9eb5f85a049c4b5ac6be365b0ef7f7b87858281d
Ville-Veikko Saari <ville-veikko.saari@cs.helsinki.fi>
2015-05-03 23:48:33 +0300

    teht43

--- web/app/app.js
+++ web/app/app.js
@@ -1,1 +1,36 @@
 // Toteuta moduulisi tänne
+var MyApp = angular.module('MyApp', ['ngRoute', 'firebase']);
+
+MyApp.service('FirebaseService', function ($firebase) {
+    var firebaseRef = new Firebase('https://fiery-fire-3593.firebaseio.com/movies')
+    var sync = $firebase(firebaseRef);
+    var movies = sync.$asArray();
+
+    this.addMovie = function (name, director, year, description) {
+        movies.$add({
+            name: name,
+            director: director,
+            year: year,
+            description: description
+        });
+    }
+    this.getMovies = function () {
+        return movies;
+    }
+});
+
+MyApp.config(function ($routeProvider) {
+    $routeProvider
+            .when('/movies', {
+                controller: 'MoviesController',
+                templateUrl: 'views/list.html'
+            })
+            .when('/movies/new', {
+                controller: 'MoviesController',
+                templateUrl: 'views/add.html'
+            })
+            .when('/', {
+                controller: 'MoviesController',
+                templateUrl: 'views/list.html'
+            })
+});
---
+++ web/app/controllers/movies_controller.js
@@ -0,0 +1,13 @@
+MyApp.controller('MoviesController', function ($scope, FirebaseService, $location) {
+    $scope.movies = FirebaseService.getMovies();
+
+    $scope.addMovie = function () {
+        FirebaseService.addMovie($scope.name, $scope.director, $scope.year, $scope.description);
+        $scope.name = '';
+        $scope.director = '';
+        $scope.year = '';
+        $scope.description = '';
+
+        $location.path('/movies');
+    };
+})
